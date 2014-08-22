b8016d37ca3048c54244045efcda5840bc4d3d9a
Orabig <benoit.chauvet@gmail.com>
2014-08-22 05:09:04 -0400

    Improved code, added --autodoc feature

--- perl/check_test.pl
+++ perl/check_test.pl
@@ -1,29 +1,22 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+
+my $Version = '1.3';
+my $PLUGIN_NAME = 'CHECK-FS';
+my $PLUGIN_DESC = q!Check filesystem (multi-OS : Linux/Windows)!;
  
 use File::Basename qw(dirname);
 use Cwd  qw(abs_path);
 use lib dirname (abs_path $0) . '/lib';
 
-use Nagios::Autoconf qw(declareArgs);
- 
-my %ARGS = declareArgs('# Autodoc : format CSV
+use Nagios::Autoconf qw(initAutoconf);
 
-# Plugin :
-# Name;Version;Description
+my %ARGS = initAutoconf("$PLUGIN_NAME;$Version;$PLUGIN_DESC", [
+			'HOST-IP;H;IP;1;0;1;IP address of host' ,
+			'FILESYSTEM;n;!/([\w\-\.]+/)*[\w\-\.]*|[A-Z]:!;1;1;0;Name of the filesystem' ,
+			'WARNING;w;!\d+[%bkmgt]?!i;0;0;0;Warning threshold (in bytes or %)' ,
+			'CRITICAL;c;!\d+[%bkmgt]?!i;0;0;0;Critical threshold (in bytes or %)' ]);
 
-CHECK-FS;1.3;Check filesystem (multi-OS : Linux/Windows)
-
-# Arguments :
-# Name;Shortcut;Format;Mandatory;Discoverable;UsedForDiscovery;Description
-
-HOST-IP;H;IP;1;0;1;IP address of host
-FILESYSTEM;n;!/([\w\-\.]+/)*[\w\-\.]*|[A-Z]:!;1;1;0;Name of the filesystem
-WARNING;w;!\d+[%bkmgt]?!i;0;0;0;Warning threshold (in bytes or %)
-CRITICAL;c;!\d+[%bkmgt]?!i;0;0;0;Critical threshold (in bytes or %)
-');
-
-$\=$/;
-print $ARGS{'HOST-IP'};
-print $ARGS{'WARNING'};
+# print $ARGS{'HOST-IP'};
+# print $ARGS{'WARNING'};
--- perl/lib/Nagios/Autoconf.pm
+++ perl/lib/Nagios/Autoconf.pm
@@ -6,34 +6,63 @@ use Getopt::Long qw(:config no_ignore_case);
  
 use Exporter qw(import);
  
-our @EXPORT_OK = qw(declareArgs);
+our @EXPORT_OK = qw(initAutoconf);
 
 my %PLUGIN;
 my @ARGUMENTS;
 
 # This function must be called at the start of the script
-sub declareArgs {
-  my ($_) = @_;
-  my $format = $1 if /# *Autodoc *: *format +(\w+)/i;
-  my ($plugin_part, $args_part)=split /# *Arguments *:? */;
-  $plugin_part=~s/^(#.*|$)\n*//mg;
-  $args_part=~s/^(#.*|$)\n*//mg;
+# It takes 2 arguments :
+#    The plugin description part, which is a string in format : "PLUGIN_NAME;PLUGIN_VERSION;PLUGIN_DESCRIPTION"
+#    An array of Argument description in format "LOGICAL_NAME;ARGUMENT_NAME;FORMAT;MANDATORY;DISCOVERABLE;USED_FOR_DISCO;DESCRIPTION"
+sub initAutoconf {
+  my ($plugin_part, $args_part) = @_;
   ($PLUGIN{'NAME'},$PLUGIN{'VERSION'},$PLUGIN{'DESC'})=split /;/, $plugin_part;
   @ARGUMENTS = map {
 	my %ARG;
 	($ARG{'NAME'},$ARG{'SHORTCUT'},$ARG{'FORMAT'},$ARG{'MANDATORY'},$ARG{'DISCO'},$ARG{'USED'},$ARG{'DESC'}) = split /;/;
 	\%ARG;
-	} split $/, $args_part;
+	} @$args_part;
 	
 	# Read the arguments
 	my $ARGS_VALUES = {};
-	GetOptions map {
-		my $name=$_->{'NAME'};
-		my $short=$_->{'SHORTCUT'};
-		"$short:s"=>\$ARGS_VALUES->{$name};
-		} @ARGUMENTS;
+	my $autoconf; my $autodoc;
+	GetOptions ( 
+		"autoconf" => \$autoconf,
+		"autodoc"  => \$autodoc ,
+		map {
+			my $name=$_->{'NAME'};
+			my $short=$_->{'SHORTCUT'};
+			"$short:s"=>\$ARGS_VALUES->{$name};
+		} @ARGUMENTS );
+		
+	# Manage --autodoc request
+	if ($autodoc) {
+		my $args_in_line = join $/,@$args_part;
+		print <<AUTODOC_END;
+# Autodoc : format CSV
+
+# Plugin :
+# Name;Version;Description
+
+$plugin_part
+
+# Arguments :
+# Name;Shortcut;Format;Mandatory;Discoverable;UsedForDiscovery;Description
+
+$args_in_line
+
+AUTODOC_END
+	exit_unknown();
+	}
 		
 	%$ARGS_VALUES;
+}
+
+sub exit_unknown {
+	my ($output)=@_;
+	print $output if defined $output;
+	exit(3);
 }
  
 1;
